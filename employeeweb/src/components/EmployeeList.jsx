ae47db73c4275f6c26fad5b84471ca73bfe37ef6
Pranav <pranavkusalkar39@gmail.com>
2026-02-20 21:48:46 +0530

    New: for the Didplay emp list

--- employeeweb/src/components/EmployeeList.jsx
+++ employeeweb/src/components/EmployeeList.jsx
@@ -1,9 +1,55 @@
-function EmployeeList() {
+import React, { useEffect, useState } from "react";
+import { getAllEmployees } from "../services/EmployeeService";
+
+function ListEmployeeComponent() {
+
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    fetchEmployees();
+  }, []);
+
+  const fetchEmployees = () => {
+    getAllEmployees()
+      .then((response) => {
+        setEmployees(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <div className="text-center mt-5">
-      <h2>Employee List Page</h2>
+    <div className="container">
+      <h2 className="text-center">Employee List</h2>
+
+      <table className="table table-bordered table-striped">
+
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Department</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {employees.map((emp) => (
+            <tr key={emp.id}>
+              <td>{emp.id}</td>
+              <td>{emp.name}</td>
+              <td>{emp.email}</td>
+              <td>{emp.phone}</td>
+              <td>{emp.dept}</td>
+            </tr>
+          ))}
+        </tbody>
+
+      </table>
     </div>
   );
 }
 
-export default EmployeeList;
+export default ListEmployeeComponent;
